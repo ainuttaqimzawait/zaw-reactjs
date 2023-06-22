978d044e59f3ede776416849641493e12467c4d8
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-06-22 20:02:08 +0700

    menambahkan isi index

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Oke sudah ditambah
         </p>
         <a
           className="App-link"
